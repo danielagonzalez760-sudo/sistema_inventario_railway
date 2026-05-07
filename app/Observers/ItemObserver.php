923d2effa81038dc46a9d30d8013446786476b4a
Shelcy0306 <shelcyrondon@pascualbravo.edu.co>
2026-05-07 17:53:13 -0500

    actualizar itemobserver

--- app/Observers/ItemObserver.php
+++ app/Observers/ItemObserver.php
@@ -8,13 +8,11 @@ use Illuminate\Support\Facades\Notification;
 
 class ItemObserver
 {
-    public function updated(Item $item)
-    {
-        if ($item->isDirty('cantidad') && $item->cantidad <= $item->umbral_minimo) {
-            try {
+   public function updated(Item $item)
+        {
+            if ($item->wasChanged('cantidad') && $item->cantidad <= $item->umbral_minimo) {
                 Notification::route('mail', config('mail.from.address'))
                     ->notify(new StockLowNotification($item->nombre, $item->cantidad));
-            } catch (\Exception $e) {}
+            }
         }
-    }
 }
