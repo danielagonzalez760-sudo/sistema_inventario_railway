1f84a48f249fdd2326ef65773672560b3238ed3e
Shelcy0306 <shelcyrondon@pascualbravo.edu.co>
2026-05-14 23:00:37 -0500

    Forzar limpieza de Apache y pausar migración

--- docker/start.sh
+++ docker/start.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
-# Asegurar que los permisos sean correctos
+# 1. Limpiar a la fuerza cualquier motor cruzado de Apache
+rm -f /etc/apache2/mods-enabled/mpm_event.load
+rm -f /etc/apache2/mods-enabled/mpm_worker.load
+a2enmod mpm_prefork
+
+# 2. Configurar el puerto dinámico de Railway
+sed -i "s/80/${PORT:-80}/g" /etc/apache2/sites-available/000-default.conf /etc/apache2/ports.conf
+
+# 3. Asegurar permisos correctos
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 
-# Limpiar y cachear configuración para producción
+# 4. Limpiar cache de Laravel
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Ejecutar migraciones automáticamente
-php artisan migrate --force
+# NOTA: Comentamos las migraciones temporalmente hasta conectar la base de datos
+# php artisan migrate --force
 
-# Configurar el puerto dinámico que asigna Railway
-sed -i "s/80/${PORT:-80}/g" /etc/apache2/sites-available/000-default.conf /etc/apache2/ports.conf
-
-# Iniciar Apache en primer plano
+# 5. Iniciar Apache
 apache2-foreground
